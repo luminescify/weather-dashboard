0b5c487de0679fe2e5f49158f5cd464ad0b69235
Leah Davis <leahdavis@Leahs-MacBook-Pro.local>
2021-10-02 21:05:30 -0400

    Search functionality built and search history populates buttons to the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var searchHistory = [];
-var apiKey = 'a4220fb97dc10c679584bc6c652f5441';
+var apiKey = '1954df614121c3c2ef68df0743988e4c';
 var rootUrl = 'https://api.openweathermap.org';
+var openCallAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=' + apiKey;
 
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
@@ -8,14 +9,41 @@ var currentWeather = document.querySelector('#today');
 var forecastWeather = document.querySelector('#forecast');
 var historyContainer = document.querySelector('#history');
 
+function loadHistory() {
+    searchHistory = JSON.parse(localStorage.getItem("city"));
+    if (searchHistory.length !== null) {
+        for (var i = 0; i < searchHistory.length; i++) {
+            var cityList = document.createElement("button");
+            cityList.setAttribute("id", searchHistory[i].savedCity);
+            cityList.setAttribute("class", "prev-search")
+    
+            cityList.textContent = searchHistory[i].savedCity;
+            historyContainer.appendChild(cityList);
+        }
+    }
+}
+
 function getLatLong(event) {
     event.preventDefault();
     console.log(event);
     var city = searchInput.value.trim();
 
-    var coordinatesUrl = rootUrl + '/geo/1.0/direct?q=' + city + "&limit=5&appid=" + apiKey;
+    var coordinatesUrl = rootUrl + '/geo/1.0/direct?q=' + city + '&limit=5&appid=' + apiKey;
     console.log(city);
 
+    // Saves searched cities to local storage
+     historyOb = {
+        savedCity: city,
+    };
+    for (var i = 0; i < searchHistory.length; i++) {
+        if (!searchHistory[i].city == searchHistory.city) {
+            searchHistory.splice(i, 1);
+        }
+    }
+    searchHistory.push(historyOb);
+    localStorage.setItem("city", JSON.stringify(searchHistory));
+
+    // Fetch's latitude and longitude data for weather location
     fetch(coordinatesUrl)
         .then(function (response) {
             return response.json();
@@ -25,12 +53,34 @@ function getLatLong(event) {
             console.log(data[0].lat, data[0].lon);
             getWeather(data[0].lat, data[0].lon);
         });
+    
+}
+
+
+
+function getWeek(lat, lon) {
+    var day1Weather = rootUrl + '/data/2.5/onecall/timemachine?lat=' + lat + '&lon=' + lon + '&dt={time}&units=imperial&appid=' + apiKey;
+    var day2Weather = rootUrl + '/data/2.5/onecall/timemachine?lat=' + lat + '&lon=' + lon + '&dt={time}&units=imperial&appid=' + apiKey;
+    var day3Weather = rootUrl + '/data/2.5/onecall/timemachine?lat=' + lat + '&lon=' + lon + '&dt={time}&units=imperial&appid=' + apiKey;
+    var day4Weather = rootUrl + '/data/2.5/onecall/timemachine?lat=' + lat + '&lon=' + lon + '&dt={time}&units=imperial&appid=' + apiKey;
+    var day5Weather = rootUrl + '/data/2.5/onecall/timemachine?lat=' + lat + '&lon=' + lon + '&dt={time}&units=imperial&appid=' + apiKey;
 
 }
 
 function getWeather(lat, lon) {
+    var weatherUrl = rootUrl + '/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey;
     console.log('inside getWeather() function');
     console.log (lat, lon);
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            //currentWeather.append(weatherUrl);
+        })
 }
 
+loadHistory();
 searchForm.addEventListener('submit', getLatLong);
