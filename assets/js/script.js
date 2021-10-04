61b7b818136335000fff70baafda0c14dc724e87
Leah Davis <ileahd96@gmail.com>
2021-10-04 19:42:35 -0400

    Fixed broken function, populated 5-day weather forecast data. Working on formatting moment dates and setting past cities to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,28 @@
-var searchHistory = [];
-var lastCity = "";
 var apiKey = '1954df614121c3c2ef68df0743988e4c';
 var rootUrl = 'https://api.openweathermap.org';
 var queryURL;
+var week = [];
 
 //DOM Selectors
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
 var currentWeather = document.querySelector('#today');
-var forecastWeather = document.querySelector('#forecast');
 var historyContainer = document.querySelector('#history');
 var citySearch = document.querySelector('#searched-city');
 var currentTemp = document.querySelector('#temp');
 var currentWind = document.querySelector('#wind');
 var currentHumidity = document.querySelector('#humidity')
 var currentUVI = document.querySelector('#uv');
-var forecastDate = document.querySelector('#forecast-date')
+var uviTitle = document.querySelector('#uv-title');
+
 
 
 // API Call for current weather in city of choice
 function getLatLong(lat, lon) {
-    console.log('inside getCurrentWeather() function');
-    console.log(lat, lon);
+    console.log('inside getWeather() function');
 }
 
-function displayWeather() {
-    var date = moment().format("MM[/]DD[/]YYYY");
-    var city = searchInput.value.trim();
-    citySearch.textContent = city + "\n" + "(" + date + ")";
-}
-
-function getCurrentWeather(event) {
-    event.preventDefault();
-    console.log(event);
+function getWeather() {
     var city = searchInput.value.trim();
     searchHistory = searchInput.value.trim();
 
@@ -45,10 +35,8 @@ function getCurrentWeather(event) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            getCurrentWeather(data[0].lat, data[0].lon);
-
-            queryURL = rootUrl + '/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey;
+            console.log(data)
+            queryURL = rootUrl + '/data/2.5/onecall?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + apiKey;
             fetch(queryURL)
                 .then(function (response) {
                     return response.json();
@@ -62,110 +50,64 @@ function getCurrentWeather(event) {
                     currentWind.textContent = "Wind Speed: " + wind + ' mph'
                     //Pulls current humidity
                     var humidity = (data.current.humidity);
-                    currentHumidity.textContent = "Humidity: " + humidity + ' %'
+                    currentHumidity.textContent = "Humidity: " + humidity + '%'
                     //Pulls current UVI
                     var uvi = (data.current.uvi);
-                    currentUVI.textContent = "UV Index: " + uvi
+                    uviTitle.textContent = "UV Index: ";
+                    currentUVI.textContent = uvi
+                    //Pulls city name next to current date
+                    var date = moment().format("MM[/]DD[/]YYYY");
+                    citySearch.textContent = city + "\n" + "(" + date + ")";
 
                     // Change UVI background color depending on safety conditions
-                    if (uvi.value <= 2) {
-                        currentUVI.css("background", "green")
-                    } else if (uvi.value > 2 && uvi.value <= 8) {
-                        currentUVI.css("background", "yellow")
-                    } else if (uvi.value > 8) {
-                        currentUVI.css("background", "red")
+                    var uviBackground = $('.uv-background');
+                    if (uvi <= 2) {
+                        console.log("123");
+                        uviBackground.css("background-color", "green")
+                    } else if (uvi > 2 && uvi <= 8) {
+                        uviBackground.css("background-color", "yellow")
+                    } else if (uvi > 8) {
+                        uviBackground.css("background-color", "red")
                     }
+
+                    //5-Day Forecast
+                    fetch(rootUrl + "/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey).then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        var forecastWeather = $('#forecast');
+                        console.log(data);
+                        for (var i = 0; i < 5; i++) {
+                            var day = {
+                                dayNum: i,
+                                temp: data.list[i].main.temp,
+                                wind: data.list[i].wind.speed,
+                                humidity: data.list[i].main.humidity
+                            }
+                            week.push(day);
+                            forecastWeather.children().eq(i).empty();
+                        }
+                        for (var i = 0; i < 5; i++) {
+                            var currentDate = moment();
+                            var weekDate = $("<h6></h6>").text(moment(currentDate, "MM[/]DD[/]YYYY").add(i + 1, 'days'));
+                            var status = $("<h6></h6>").text("Weather Conditions")
+                            var weekTemp = $("<h6></h6>").text(week[i].temp);
+                            var weekWind = $("<h6></h6>").text(week[i].wind);
+                            var weekHumid = $("<h6></h6>").text(week[i].humidity);
+                            console.log(forecastWeather.children()[i]);
+                            forecastWeather.children().eq(i).append(weekDate, status, weekWind, weekTemp, weekHumid);
+                        }
+                    })
                 })
         })
 }
-displayWeather();
-
-var getForecast = (event) => {
-    var city = $(searchInput).val();
-    var queryURL = rootUrl + '/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey;
-
-    fetch(queryURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((response) => {
-            for (var i = 0; i < response.list.length; i++) {
-                var dateData = response.list[i];
-                var dayTimeUTC = dateData.dt;
-                var timeZoneOffset = response.city.timezone;
-                var timeZoneOffsetHours = timeZoneOffset / 60 / 60;
-                var currentMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
-                var iconURL = rootUrl + "/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + current.dt + "&appid=" + apiKey;
-            }
-        })
-}
-
-// Saves city to local storage
-var saveCity = (newCity) => {
-    var cityExists = false;
-    for (i = 0; i < localStorage.length; i++) {
-        if (localStorage["city" + i] === newCity) {
-            cityExists = true;
-            break;
-        }
-    }
-    if (cityExists === false) {
-        localStorage.setItem('city' + localStorage.length, newCity);
-    }
-}
-
-var renderCity = () => {
-    $('#city-results').empty();
-
-    if (localStorage.length === 0) {
-        if (lastCity) {
-            $(searchInput).attr("value", lastCity);
-        } else {
-            $(searchInput).attr("value", "Orlando")
-        }
-    } else {
-        var lastCityKey = "city" + (localStorage.length - 1);
-        lastCity = localStorage.getItem(lastCityKey);
-        $(searchInput).attr("value", lastCity);
-        for (var i = 0; i < localStorage.length; i++) {
-            var city = localStorage.getItem("city" + i);
-            var cityEl;
-            if (searchHistory === "") {
-                searchHistory = lastCity;
-            }
-            if (city === searchHistory) {
-                cityEl = `<button type="button" class=" list-group-item list-group-item-action active">${city}</button></li>`
-            } else {
-                cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`
-            }
-            $(searchInput).prepend(cityEl);
-        }
-        if (localStorage.length > 0) {
-            $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'))
-        } else {
-            $('#clear-storage').html('');
-        }
-    }
-}
-
-$('#city-results').on("click", (event) => {
-    event.preventDefault();
-    $(searchInput).val(event.target.textContent);
-    searchHistory = $(searchInput).val();
-    getCurrentWeather(event);
-})
 
 $('#search-button').on("click", (event) => {
     event.preventDefault();
-    searchHistory = $(searchInput).val();
-    getCurrentWeather(event);
+    getLatLong();
+    getWeather();
 })
 
 $("#clear-storage").on("click", (event) => {
     localStorage.clear();
-    renderCity();
 })
-
-renderCity();
-
-searchForm.addEventListener('submit', getLatLong)
